83956589df4c6c3f8f1a9ec062ffb238cc28eacd
Jonathan Manes <manesjona@gmail.com>
2023-04-21 09:58:30 +0200

    styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {FaBars} from "react-icons/fa";
+import {GiCrossedBones} from "react-icons/gi";
 
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
@@ -7,16 +8,20 @@ const Navbar = () => {
     return (
         <header>
             <nav className="fixed top-0 z-20 w-full justify-between px-2 py-2 bg-indigo-900 text-gray-50">
-                <div className="container  mx-auto flex flex-wrap items-center justify-between">
+                <div className="container  mx-auto flex flex-wrap  justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase hover:text-orange-500"
                            href="/home">Todo Web App
                         </a>
                         <button
-                            className="cursor-pointer text-xl leading-none py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            className="cursor-pointer text-xl leading-none py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none hover:text-orange-500"
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}>
-                            <FaBars/>
+                            {navbarOpen ? (
+                                <GiCrossedBones/>
+                            ) : (
+                                <FaBars/>
+                            )}
                         </button>
                     </div>
                     <div
@@ -25,30 +30,29 @@ const Navbar = () => {
                             (navbarOpen ? " flex" : " hidden")
                         }
                         id="navbar">
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto justify-center">
-
+                        <ul className={"flex flex-col lg:flex-row list-none lg:ml-auto justify-center" + (navbarOpen ? " w-full" : "")}>
                             <li>
-                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:opacity-75 cursor-pointer'}
+                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:text-orange-500 cursor-pointer w-full'}
                                    href={'/home'}>Tâches
                                 </a>
                             </li>
                             <li>
-                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:opacity-75 cursor-pointer'}
+                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:text-orange-500 cursor-pointer w-full'}
                                    href={'/calendar'}>Calendrier
                                 </a>
                             </li>
                             <li>
-                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:opacity-75 cursor-pointer'}
+                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:text-orange-500 cursor-pointer w-full'}
                                    href={'/courses'}>Courses
                                 </a>
                             </li>
                             <li>
-                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:opacity-75 cursor-pointer'}
+                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:text-orange-500 cursor-pointer w-full'}
                                    href={'/promises'}>Promesses
                                 </a>
                             </li>
                             <li>
-                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:opacity-75 cursor-pointer'}
+                                <a className={'px-3 py-2 flex text-xs uppercase font-bold leading-snug hover:text-orange-500 cursor-pointer w-full'}
                                    href={'/logout'}>Logout
                                 </a>
                             </li>
